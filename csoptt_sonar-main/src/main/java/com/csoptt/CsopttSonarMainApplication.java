319fab155f42b53b8cdb410c456a37b5047153f4
刘子西 <Liuzx1337@163.com>
2018-12-20 14:48:11 +0800

    First commit

---
+++ csoptt_sonar-main/src/main/java/com/csoptt/CsopttSonarMainApplication.java
@@ -0,0 +1,14 @@
+package com.csoptt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsopttSonarMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsopttSonarMainApplication.class, args);
+	}
+
+}
+
